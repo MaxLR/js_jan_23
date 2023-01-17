e191e99ab3a5105b63db35ed54d0875178a0bf9b
Max R <max.l.rauchman@gmail.com>
2023-01-17 18:09:04 -0500

    Axios & useEffect

--- flashcards/src/App.jsx
+++ flashcards/src/App.jsx
@@ -1,11 +1,14 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import flashcardsData from './data/flashcards.json';
 import './App.css';
-import { useState } from 'react';
 import NewFlashcard from './components/NewFlashcard';
 import AllFlashcards from './components/AllFlashcards';
 
 const App = () => {
     const [flashcards, setFlashcards] = useState([])
+    const [flashcardCount, setFlashcardCount] = useState(10)
+    const [questionType, setQuestionType] = useState(18)
 
     const handleFlip = (event, selectedCardIdx) => {
         console.log("Flipped")
@@ -22,19 +25,32 @@ const App = () => {
         setFlashcards(updatedCards);
     }
 
-    const getFlashcards = (event) => {
-        fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=boolean")
-            .then(res => {
-                console.log(res)
-                res.json()
-                    .then(res => {
-                        console.log(res.results);
-                        setFlashcards(res.results);
-                    })
-            }).catch(err => {
-                console.log(err.json())
-            })
-    }
+    //useEffect takes 2 arguments, a function to be run, & an array of variables that should trigger it to run
+    useEffect(() => {
+        //using axios instead of fetch
+        axios.get(`https://opentdb.com/api.php?amount=${flashcardCount}&category=${questionType}&difficulty=medium&type=boolean`).then(res => {
+            //don't need to call .json() b/c axios handles json conversion for us
+            console.log(res.data.results)
+            setFlashcards(res.data.results)
+        }).catch(err => {
+            console.log(err)
+        })
+
+
+        // fetch(`https://opentdb.com/api.php?amount=${flashcardCount}&category=${questionType}&difficulty=medium&type=boolean`)
+        //     .then(res => {
+        //         console.log(res)
+        //         res.json()
+        //             .then(res => {
+        //                 console.log(res.results);
+        //                 setFlashcards(res.results);
+        //             })
+        //     }).catch(err => {
+        //         console.log(err.json())
+        //     })
+    }, [flashcardCount, questionType])
+    //   ^  this array is the list of variables that useEffect is "watching"
+    //whenever one of these variables change, we run our useEffect function again
 
     const handleDelete = (event, idxToDelete) => {
         //stops any onclicks from parents
@@ -58,9 +74,12 @@ const App = () => {
                 <h1>Programming Flash Cards</h1>
                 <hr />
             </header>
-
-            <button onClick={getFlashcards}>Get Flashcards</button>
-
+            <label>Amount of Flashcards:</label>
+            <input type="number" onChange={e => setFlashcardCount(e.target.value)} value={flashcardCount}/>
+            <label>Question Type:</label>
+            <input type="number" onChange={e => setQuestionType(e.target.value)} value={questionType}/>
+            <br />
+            <br />
             <NewFlashcard addCard={addCardToList}/>
 
             <AllFlashcards flashcards={flashcards} handleFlip={handleFlip} handleDelete={handleDelete}/>
